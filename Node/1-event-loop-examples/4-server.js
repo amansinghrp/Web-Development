d6081549e12db301c67702e6e261fbf6ff27cf77
Aman Singh <rajputaman6554@gmail.com>
2025-02-22 00:14:05 +0530

    Learnt about async Patterns in NodeJS

--- Node/1-event-loop-examples/4-server.js
+++ Node/1-event-loop-examples/4-server.js
@@ -1,8 +1,11 @@
+const { log } = require('console');
 const http = require('http')
-
+var cnt = 0
 const server = http.createServer((req, res)=>{
+    log("Request event: " + cnt + " URL: " + req.url);
     res.write("Hello guys, welcome to event loop classes");
     res.end();
+    cnt++;
 })
 
 server.listen(5000, () =>{
